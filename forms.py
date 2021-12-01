4259d587363f8ff289a16669a74137fac415031a
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-12-02 00:32:29 +0530

    Update forms.py

--- forms.py
+++ forms.py
@@ -4,6 +4,6 @@ from wtforms import StringField, SubmitField
 class SignUpForm(FlaskForm):
     name = StringField('Enter your Name')
     age = StringField('Enter your age')
-    passion = StringField('Enter your Passion')
+    passion = StringField('Enter the Application/Website name')
     master_password = StringField('Enter the Master Password')
     submit = SubmitField('Submit')
