00e107ac01ab6a9944efff9fabc8b0fc63c12498
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-12-01 11:59:39 +0530

    fixes

--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
 class SignUpForm(FlaskForm):
-    name = StringField('Customer Name')
+    name = StringField('Enter your Name')
     age = StringField('Enter your age')
     passion = StringField('Enter your Passion')
     master_password = StringField('Enter the Master Password')
