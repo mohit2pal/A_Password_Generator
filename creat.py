1969c9673720e1f56d4f395493604c7fedb2b8cc
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-12-02 00:28:51 +0530

    creat fixed

--- creat.py
+++ creat.py
@@ -1,6 +1,6 @@
 
 def crat(x,y,z):
-    L1 = ("\n", 'x_choice="', x,'"' "\n", 'y_choice="', y,'"' "\n", 'z_choice="', z,'"'  )
+    L1 = ("\n", x, "\n", y, "\n", z )
     # L2 = ( )
     # L3 = ( )
     f1=open('1.txt','a')
