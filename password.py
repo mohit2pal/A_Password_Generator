b7e01185aa2e5f49d7bb26ff93b364801f695952
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2021-12-10 14:43:31 +0530

    Create password.py

---
+++ password.py
@@ -0,0 +1,12 @@
+import random
+
+x = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyl][\][\]\;./0123456789'
+
+passnew= ''
+
+y = int(input("Enter the length of the password"))
+
+for i in range(y):
+    passnew = passnew + random.choice(x)
+
+print("The generated password is ", passnew)
