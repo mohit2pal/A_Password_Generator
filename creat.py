360127e800ed900a8e97b1cbe2677a8a92d28995
SUBHA BAL PAL <91749681+mohit2pal@users.noreply.github.com>
2022-02-18 03:03:49 +0530

    edits

--- creat.py
+++ creat.py
@@ -1,11 +1,9 @@
-
-def crat(x,y,z):
-    L1 = ("\n", x, "\n", y, "\n", z )
+def crat(x, y, z):
+    L1 = ("\n", x, "\n", y, "\n", z)
     # L2 = ( )
     # L3 = ( )
-    f1=open('1.txt','a')
+    f1 = open("1.txt", "a")
     f1.writelines(L1)
     # f1.writelines(L2)
     # f1.writelines(L3)
     f1.close()
-    
--- forms.py
+++ forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+
 class SignUpForm(FlaskForm):
-    name = StringField('Enter your Name')
-    age = StringField('Enter your age')
-    passion = StringField('Enter the Application/Website name')
-    master_password = StringField('Enter the Master Password')
-    submit = SubmitField('Submit')
+    name = StringField("Enter your Name")
+    age = StringField("Enter your age")
+    passion = StringField("Enter the Application/Website name")
+    master_password = StringField("Enter the Master Password")
+    submit = SubmitField("Submit")
